5ec015898f4505d40a9af4f89cc75f617ec0a2a8
Mohamamd Hammad <hammad.scripted@gmail.com>
2026-03-07 16:07:21 +0530

    enable h2 console

--- src/main/java/org/example/springsecuritydemo/SecurityConfig.java
+++ src/main/java/org/example/springsecuritydemo/SecurityConfig.java
@@ -6,6 +6,8 @@ import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.annotation.web.configurers.HeadersConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
@@ -21,10 +23,10 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http) throws Exception {
         http
-                .authorizeHttpRequests((requests) -> requests
+                .authorizeHttpRequests((requests) -> requests.requestMatchers("/h2-console/**").permitAll()
                         .anyRequest().authenticated() // All URLs are protected
                         // to make our api stateless so that they can't remember previous data
-                ).sessionManagement(session->session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                ).sessionManagement(session->session.sessionCreationPolicy(SessionCreationPolicy.STATELESS)).headers(headers->headers.frameOptions(HeadersConfigurer.FrameOptionsConfig::sameOrigin)).csrf(AbstractHttpConfigurer::disable)
                 // Enables the default login page
 //                .formLogin(Customizer.withDefaults())
                 // Enables "Basic" authentication for API testing (Postman/curl)
