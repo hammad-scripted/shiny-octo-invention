9c5481449fd256a3923677f5929863b895b7e88b
Mohamamd Hammad <hammad.scripted@gmail.com>
2026-03-08 23:59:06 +0530

    utility methods for jwt

---
+++ src/main/java/org/example/springsecuritydemo/jwt/JwtUtils.java
@@ -0,0 +1,34 @@
+package org.example.springsecuritydemo.jwt;
+
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.io.Decoders;
+import io.jsonwebtoken.security.Keys;
+import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.stereotype.Component;
+
+import javax.crypto.SecretKey;
+import java.util.Date;
+
+@Component
+public class JwtUtils
+{
+    public String getJwtFromHeader(HttpServletRequest request){
+        String bearerToken=request.getHeader("Authorization");
+        if(bearerToken!=null && bearerToken.startsWith("Bearer ")){
+            return bearerToken.substring(7);
+        }
+        return null;
+    }
+
+    public  String generateTokenFromUsername(UserDetails userDetails){
+        String username=userDetails.getUsername();
+        return Jwts.builder().subject(username).issuedAt(new Date()).expiration(new Data(new Date()).getTime()+jwtExpirationMs).signWith(key()).compact();
+    }
+    public String getUserNameFromJwtToken(String token){
+        return Jwts.parser().verifyWith((SecretKey) key()).build().parseSignedClaims(token).getPayload().getSubject();
+    }
+    private Key key(){
+        return Keys.hmacShaKeyFor(Decoders.BASE64.decode(jwtSecret));
+    }
+}
