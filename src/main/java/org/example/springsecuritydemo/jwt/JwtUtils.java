c01e5554824b0457f8f943bdab0fcdcda7121045
Mohamamd Hammad <hammad.scripted@gmail.com>
2026-03-09 00:08:03 +0530

    completed jwt utility methods

--- src/main/java/org/example/springsecuritydemo/jwt/JwtUtils.java
+++ src/main/java/org/example/springsecuritydemo/jwt/JwtUtils.java
@@ -1,34 +1,52 @@
 package org.example.springsecuritydemo.jwt;
-
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.io.Decoders;
 import io.jsonwebtoken.security.Keys;
 import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Component;
 
 import javax.crypto.SecretKey;
+import java.security.Key;
 import java.util.Date;
 
 @Component
-public class JwtUtils
-{
-    public String getJwtFromHeader(HttpServletRequest request){
-        String bearerToken=request.getHeader("Authorization");
-        if(bearerToken!=null && bearerToken.startsWith("Bearer ")){
+public class JwtUtils {
+    @Value("${spring.app.jwtSecret}")
+    private String jwtSecret;
+
+    @Value("${spring.app.jwtExpiratioMs}")
+    private int jwtExpirationMs;
+
+    public String getJwtFromHeader(HttpServletRequest request) {
+        String bearerToken = request.getHeader("Authorization");
+        if (bearerToken != null && bearerToken.startsWith("Bearer ")) {
             return bearerToken.substring(7);
         }
         return null;
     }
 
-    public  String generateTokenFromUsername(UserDetails userDetails){
-        String username=userDetails.getUsername();
-        return Jwts.builder().subject(username).issuedAt(new Date()).expiration(new Data(new Date()).getTime()+jwtExpirationMs).signWith(key()).compact();
+    public String generateTokenFromUsername(UserDetails userDetails) {
+        String username = userDetails.getUsername();
+        return Jwts.builder().subject(username).issuedAt(new Date()).expiration(new Data(new Date()).getTime() + jwtExpirationMs).signWith(key()).compact();
     }
-    public String getUserNameFromJwtToken(String token){
+
+    public String getUserNameFromJwtToken(String token) {
         return Jwts.parser().verifyWith((SecretKey) key()).build().parseSignedClaims(token).getPayload().getSubject();
     }
-    private Key key(){
+
+    private Key key() {
         return Keys.hmacShaKeyFor(Decoders.BASE64.decode(jwtSecret));
+    }
+
+    public boolean validateJwtToken(String authToken) {
+        try {
+            System.out.println("Validate");
+            Jwts.parser().verifyWith((SecretKey) key()).build().parseSignedClaims(authToken);
+            return true;
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
     }
 }
